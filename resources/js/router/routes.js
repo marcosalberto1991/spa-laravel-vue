9871f24b6931365bad4cf15bf1d0bcbe610b50a2
Marcos Alberto Saavedra <once.marcos.alberto@gmail.com>
2020-06-24 18:58:42 -0500

    Update routes.js
    webpackChunkName generaba un error en la vista dejando de servir la pagina

--- resources/js/router/routes.js
+++ resources/js/router/routes.js
@@ -1,6 +1,6 @@
-// function page (path) {
-//   return () => import( webpackChunkName: ''  `~/pages/${path}`).then(m => m.default || m)
-// }
+ function page (path) {
+   return () => import( /* webpackChunkName: '' */  `~/pages/${path}`).then(m => m.default || m)
+ }
 
 export default [
   { path: '/', name: 'welcome', component: page('welcome.vue') },
