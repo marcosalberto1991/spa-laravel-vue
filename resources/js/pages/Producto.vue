7624dcd58dd743fe28d7f9bf31e4d1d6c68ab75e
Marcos Alberto Saavedra <once.marcos.alberto@gmail.com>
2020-06-27 09:52:58 -0500

    2020-06-27

--- resources/js/pages/Producto.vue
+++ resources/js/pages/Producto.vue
@@ -1,21 +1,46 @@
 <template>
-  <card :title="$t('home')">
-    {{ $t('you_are_logged_in') }}
-    
-    
+  <card :title="$t('Producto')">
+    {{ $t("you_are_logged_in") }}
   </card>
 </template>
 
 <script>
+import Vue from "vue";
+import axios from 'axios'
+
 //import FetchUsers from "~/components/FetchUsers"
 export default {
-	components : {
-	//	FetchUsers
-	},
-  middleware: 'auth',
+  components: {
+    //	FetchUsers
+  },
+  middleware: "auth",
 
-  metaInfo () {
-    return { title: this.$t('Producto') }
+  metaInfo() {
+    return { title: this.$t("Producto") };
+  },
+  data() {
+    return {
+      formulario_Producto: false,
+      productos_all: [],
+      editar_dato: false,
+      data_foraneo_proveedor_id: [],
+      datas: []
+    };
+  },
+  mounted() {
+    //this.consulta();
+    alert("paso");
+
+    this.data_foraneo();
+  },
+  methods: { 
+    data_foraneo() {
+      axios.get("api/Producto/create").then(response => {
+        this.productos_all = response.data;
+        this.data_foraneo_proveedor_id = response.data.proveedor_id;
+        alert('consulta')
+      });
+    } //fin de
   }
-}
+};
 </script>
--- routes/api.php
+++ routes/api.php
@@ -26,6 +26,9 @@ Route::group(['middleware' => 'auth:api'], function () {
 
     Route::patch('settings/profile', 'Settings\ProfileController@update');
     Route::patch('settings/password', 'Settings\PasswordController@update');
+
+    Route::get('Producto/consulta', 'ProductoController@consulta');
+    Route::resource('Producto','ProductoController');
 });
 
 Route::group(['middleware' => 'guest:api'], function () {
@@ -40,4 +43,6 @@ Route::group(['middleware' => 'guest:api'], function () {
 
     Route::post('oauth/{driver}', 'Auth\OAuthController@redirectToProvider');
     Route::get('oauth/{driver}/callback', 'Auth\OAuthController@handleProviderCallback')->name('oauth.callback');
+
+
 });
